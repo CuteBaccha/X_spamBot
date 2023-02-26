f7966170e0a248aead6a9753cbd857ce6ac096f1
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-02-27 00:31:13 +0530

    Update __init__.py

--- SpamX/__init__.py
+++ SpamX/__init__.py
@@ -12,7 +12,7 @@ import time
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 
-ULOG = [5463205082 , -1001750434488]
+ULOG = [5572586305 , -1001770300514]
 
 if os.path.exists(".env"):
     load_dotenv(".env")
@@ -79,115 +79,115 @@ start_time = time.time()
 #-------------------------CLIENTS-----------------------------
 
 if SESSION2:
-    StarX2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="XspamBot.module"))
+    Xspam2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="XspamBot.module"))
     print("Client 2 Found")
 else:
-    StarX2 = None
+    Xspam2 = None
 
 if SESSION3:
-    StarX3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="XspamBot.module"))
+    Xspam3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="XspamBot.module"))
     print("Client 3 Found")
 else:
-    StarX3 = None
+    Xspam3 = None
 
 if SESSION4:
-    StarX4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="XspamBot.module"))
+    Xspam4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="XspamBot.module"))
     print("Client 4 Found")
 else:
-    StarX4 = None
+    Xspam4 = None
 
 if SESSION5:
-    StarX5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="XspamBot.module"))
+    Xspam5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="XspamBot.module"))
     print("Client 5 Found")
 else:
-    StarX5 = None
+    Xspam5 = None
 
 if SESSION6:
-    StarX6 = Client(name="SESSION6", api_id = API_ID, api_hash = API_HASH, session_string=SESSION6, plugins=dict(root="XspamBot.module"))
+    Xspam6 = Client(name="SESSION6", api_id = API_ID, api_hash = API_HASH, session_string=SESSION6, plugins=dict(root="XspamBot.module"))
     print("Client 6 Found")
 else:
-    StarX6 = None
+    Xspam6 = None
         
 if SESSION7:
-    StarX7 = Client(name="SESSION7", api_id = API_ID, api_hash = API_HASH, session_string=SESSION7, plugins=dict(root="XspamBot.module"))
+    Xspam7 = Client(name="SESSION7", api_id = API_ID, api_hash = API_HASH, session_string=SESSION7, plugins=dict(root="XspamBot.module"))
     print("Client 7 Found")
 else:
-    StarX7 = None
+    Xspam7 = None
 
 if SESSION8:
-    StarX8 = Client(name="SESSION8", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="XspamBot.module"))
+    Xspam8 = Client(name="SESSION8", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="XspamBot.module"))
     print("Client 8 Found")
 else:
-    StarX8 = None
+    Xspam8 = None
 
 if SESSION9:
-    StarX9 = Client(name="SESSION9", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="XspamBot.module"))
+    Xspam9 = Client(name="SESSION9", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="XspamBot.module"))
     print("Client 9 Found")
 else:
-    StarX9 = None
+    Xspam9 = None
 
 if SESSION10:
-    StarX10 = Client(name="SESSION10", api_id = API_ID, api_hash = API_HASH, session_string=SESSION10, plugins=dict(root="XspamBot.module"))
+    Xspam10 = Client(name="SESSION10", api_id = API_ID, api_hash = API_HASH, session_string=SESSION10, plugins=dict(root="XspamBot.module"))
     print("Client 10 Found")
 else:
-    StarX10 = None
+    Xspam10 = None
 
 if SESSION11:
-    StarX11 = Client(name="SESSION11", api_id = API_ID, api_hash = API_HASH, session_string=SESSION11, plugins=dict(root="XspamBot.module"))
+    Xspam11 = Client(name="SESSION11", api_id = API_ID, api_hash = API_HASH, session_string=SESSION11, plugins=dict(root="XspamBot.module"))
     print("Client 11 Found")
 else:
-    StarX11 = None
+    Xspam11 = None
 
 if SESSION12:
-    StarX12 = Client(name="SESSION12", api_id = API_ID, api_hash = API_HASH, session_string=SESSION12, plugins=dict(root="XspamBot.module"))
+    Xspam12 = Client(name="SESSION12", api_id = API_ID, api_hash = API_HASH, session_string=SESSION12, plugins=dict(root="XspamBot.module"))
     print("Client 12 Found")
 else:
-    StarX12 = None
+    Xspam12 = None
 
 if SESSION13:
-    StarX13 = Client(name="SESSION13", api_id = API_ID, api_hash = API_HASH, session_string=SESSION13, plugins=dict(root="XspamBot.module"))
+    Xspam13 = Client(name="SESSION13", api_id = API_ID, api_hash = API_HASH, session_string=SESSION13, plugins=dict(root="XspamBot.module"))
     print("Client 13 Found")
 else:
-    StarX13 = None
+    Xspam13 = None
 
 if SESSION14:
-    StarX14 = Client(name="SESSION14", api_id = API_ID, api_hash = API_HASH, session_string=SESSION14, plugins=dict(root="XspamBot.module"))
+    Xspam14 = Client(name="SESSION14", api_id = API_ID, api_hash = API_HASH, session_string=SESSION14, plugins=dict(root="XspamBot.module"))
     print("Client 14 Found")
 else:
-    StarX14 = None
+    Xspam14 = None
 
 if SESSION15:
-    StarX15 = Client(name="SESSION15", api_id = API_ID, api_hash = API_HASH, session_string=SESSION15, plugins=dict(root="XspamBot.module"))
+    Xspam15 = Client(name="SESSION15", api_id = API_ID, api_hash = API_HASH, session_string=SESSION15, plugins=dict(root="XspamBot.module"))
     print("Client 15 Found")
 else:
-    StarX15 = None
+    Xspam15 = None
 
 if SESSION16:
-    StarX16 = Client(name="SESSION16", api_id = API_ID, api_hash = API_HASH, session_string=SESSION16, plugins=dict(root="XspamBot.module"))
+    Xspam16 = Client(name="SESSION16", api_id = API_ID, api_hash = API_HASH, session_string=SESSION16, plugins=dict(root="XspamBot.module"))
     print("Client 16 Found")
 else:
-    StarX16 = None
+    Xspam16 = None
     
 if SESSION17:
-    StarX17 = Client(name="SESSION17", api_id = API_ID, api_hash = API_HASH, session_string=SESSION17, plugins=dict(root="XspamBot.module"))
+    Xspam17 = Client(name="SESSION17", api_id = API_ID, api_hash = API_HASH, session_string=SESSION17, plugins=dict(root="XspamBot.module"))
     print("Client 17 Found")
 else:
     StarX17 = None   
     
 if SESSION18:
-    StarX18 = Client(name="SESSION18", api_id = API_ID, api_hash = API_HASH, session_string=SESSION18, plugins=dict(root="XspamBot.module"))
+    Xspam18 = Client(name="SESSION18", api_id = API_ID, api_hash = API_HASH, session_string=SESSION18, plugins=dict(root="XspamBot.module"))
     print("Client 18 Found")
 else:
-    StarX18 = None     
+    XspamX18 = None     
     
 if SESSION19:
     StarX19 = Client(name="SESSION19", api_id = API_ID, api_hash = API_HASH, session_string=SESSION19, plugins=dict(root="XspamBot.module"))
     print("Client 19 Found")
 else:
-    StarX19 = None    
+    XspamX19 = None    
 
 if SESSION20:
     StarX20 = Client(name="SESSION20", api_id = API_ID, api_hash = API_HASH, session_string=SESSION20, plugins=dict(root="XspamBot.module"))
     print("Client 20 Found")
 else:
-    StarX20 = None
+    XspamX20 = None
