ba0ab849ab62e08fa1e6262de7ab9c93ffa77fa5
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-02-27 00:16:11 +0530

    Update profile.py

--- SpamX/module/profile.py
+++ SpamX/module/profile.py
@@ -1,4 +1,4 @@
-# Star boi X - Telegram Projects
+# ansh - Telegram Projects
 # (c) 2022 - 2023 Star
 # Don't Kang Bitch -!
 
@@ -7,17 +7,17 @@
 import os
 import sys
 from random import choice
-from SpamX import (OWNER_ID, HNDLR, SUDO_USERS, hl, LOGS_CHANNEL)
+from XspamBot import (OWNER_ID, HNDLR, SUDO_USERS, hl, LOGS_CHANNEL)
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from SpamX.data import *
 
 Usage = f"**❌ Wrong Usage ❌** \n Type: `{HNDLR}help owner`"
-Media = "SpamX/downloads/Profile.jpg"
+Media = "XspamBot/downloads/Profile.jpg"
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command(["setpic"], prefixes=HNDLR))
 @Client.on_message(filters.me & filters.command(["setpic"], prefixes=HNDLR))
-async def setpic(xspam: Client, e: Message):
+async def setpic(XspamBot: Client, e: Message):
      replied = e.reply_to_message
      if (replied and replied.media and (replied.photo or (replied.document and "image" in replied.document.mime_type))):
             await xspam.download_media(message=replied, file_name=Media)
@@ -63,11 +63,11 @@ async def setbio(xspam: Client, e: Message):
       Rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
       if len(Rizoel) == 1:
           xd = str(Rizoel[0])
-          ok = await xspam.get_me()
+          ok = await XspamBot.get_me()
           nam = ok.first_name
           nam2 = ok.last_name
           try:
-             await xspam.update_profile(first_name=nam, last_name=nam2, bio=xd)
+             await XspamBot.update_profile(first_name=nam, last_name=nam2, bio=xd)
              await e.reply_text(f"**Profile Bio Changed Successfully !** \n\n **New Bio**: {xd}")
           except Exception as ex:
             await e.reply_text(f"**Error !!** \n\n {ex}")
