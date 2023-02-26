26639aa063ad0fa93ec288eeef7116af428bbb23
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-02-27 00:25:59 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -1,4 +1,4 @@
-# Star boi - Telegram Projects
+# ansh - Telegram Projects
 # (c) 2022 - 2024 Star
 # Don't Kang Bitch -!
 
@@ -13,86 +13,86 @@ from . import (StarX, StarX2, StarX3, StarX4, StarX5,
 
 if StarX:
     StarX.start()
-    StarX.join_chat("Star_X_Network")
-    StarX.join_chat("Best_FriendsFor_Ever")
+    StarX.join_chat("XD_CUTETY")
+    StarX.join_chat("ISHQ00_I")
 if StarX2:
     StarX2.start()
-    StarX2.join_chat("Star_X_Network")
-    StarX2.join_chat("Best_FriendsFor_Ever")
+    StarX2.join_chat("XD_CUTETY")
+    StarX2.join_chat("ISHQ00_I")
 if StarX3:
     StarX3.start()
-    StarX3.join_chat("Star_X_Network")
-    StarX3.join_chat("Best_FriendsFor_Ever")
+    StarX3.join_chat("XD_CUTETY")
+    StarX3.join_chat("ISHQ00_I")
 if StarX4:
     StarX4.start()
-    StarX4.join_chat("Star_X_Network")
-    StarX4.join_chat("Best_FriendsFor_Ever")
+    StarX4.join_chat("XD_CUTETY")
+    StarX4.join_chat("ISHQ00_I")
 if StarX5:
     StarX5.start()
-    StarX5.join_chat("Star_X_Network")
-    StarX5.join_chat("Best_FriendsFor_Ever")
+    StarX5.join_chat("XD_CUTETY")
+    StarX5.join_chat("ISHQ00_I")
 if StarX6:
     StarX6.start()
-    StarX6.join_chat("Star_X_Network")
-    StarX6.join_chat("Best_FriendsFor_Ever")
+    StarX6.join_chat("XD_CUTETY")
+    StarX6.join_chat("ISHQ00_I")
 if StarX7:
     StarX7.start()
-    StarX7.join_chat("Star_X_Network")
-    StarX7.join_chat("Best_FriendsFor_Ever")
+    StarX7.join_chat("XD_CUTETY")
+    StarX7.join_chat("ISHQ00_I")
 if StarX8:
     StarX8.start()
-    StarX8.join_chat("Star_X_Network")
-    StarX8.join_chat("Best_FriendsFor_Ever")
+    StarX8.join_chat("XD_CUTETY")
+    StarX8.join_chat("ISHQ00_I")
 if StarX9:
     StarX9.start()
-    StarX9.join_chat("Star_X_Network")
-    StarX9.join_chat("Best_FriendsFor_Ever")
+    StarX9.join_chat("XD_CUTETY")
+    StarX9.join_chat("ISHQ00_I")
 if StarX10:
     StarX10.start()
-    StarX10.join_chat("Star_X_Network")
-    StarX10.join_chat("Best_FriendsFor_Ever")
+    StarX10.join_chat("XD_CUTETY")
+    StarX10.join_chat("ISHQ00_I")
 if StarX11:
     StarX11.start()
-    StarX11.join_chat("Star_X_Network")
-    StarX11.join_chat("Best_FriendsFor_Ever")
+    StarX11.join_chat("XD_CUTETY")
+    StarX11.join_chat("ISHQ00_I")
 if StarX12:
     StarX12.start()
-    StarX12.join_chat("Star_X_Network")
-    StarX12.join_chat("Best_FriendsFor_Ever")
+    StarX12.join_chat("XD_CUTETY")
+    StarX12.join_chat("ISHQ00_I")
 if StarX13:
     StarX13.start()
-    StarX13.join_chat("Star_X_Network")
-    StarX13.join_chat("Best_FriendsFor_Ever")
+    StarX13.join_chat("XD_CUTETY")
+    StarX13.join_chat("ISHQ00_I")
 if StarX14:
     StarX14.start()
-    StarX14.join_chat("Star_X_Network")
-    StarX14.join_chat("Best_FriendsFor_Ever")
+    StarX14.join_chat("XD_CUTETY")
+    StarX14.join_chat("ISHQ00_I")
 if StarX15:
     StarX15.start()
-    StarX15.join_chat("Star_X_Network")
-    StarX15.join_chat("Best_FriendsFor_Ever")
+    StarX15.join_chat("XD_CUTETY")
+    StarX15.join_chat("ISHQ00_I")
 if StarX16:
     StarX16.start()
-    StarX16.join_chat("Star_X_Network")
-    StarX16.join_chat("Best_FriendsFor_Ever")
+    StarX16.join_chat("XD_CUTETY")
+    StarX16.join_chat("ISHQ00_I")
 if StarX17:
     StarX17.start()
-    StarX17.join_chat("Star_X_Network")
-    StarX17.join_chat("Best_FriendsFor_Ever")
+    StarX17.join_chat("XD_CUTETY")
+    StarX17.join_chat("ISHQ00_I")
 if StarX18:
     StarX18.start()
-    StarX18.join_chat("Star_X_Network")
-    StarX18.join_chat("Best_FriendsFor_Ever")
+    StarX18.join_chat("XD_CUTETY")
+    StarX18.join_chat("ISHQ00_I")
 if StarX19:
     StarX19.start()
-    StarX19.join_chat("Star_X_Network")
-    StarX19.join_chat("Best_FriendsFor_Ever")
+    StarX19.join_chat("XD_CUTETY")
+    StarX19.join_chat("ISHQ00_I")
 if StarX20:
     StarX20.start()
-    StarX20.join_chat("Star_X_Network")
-    StarX20.join_chat("Best_FriendsFor_Ever")
+    StarX20.join_chat("XD_CUTETY")
+    StarX20.join_chat("ISHQ00_I")
 
 print("Your Spam Bots Successfully Deployed ✅")
-print("Visit -! @Star_X_Network")
+print("Visit -! XD_CUTETY")
 
 idle()
